3d61af8f78c90108d18b2941f86d816052a6d58f
Twelvelines <garcianofear@icloud.com>
2017-06-20 16:18:12 +0800

    deleted some activities

--- app/src/main/java/com/blakequ/androidblemanager/ui/MainActivity.java
+++ app/src/main/java/com/blakequ/androidblemanager/ui/MainActivity.java
@@ -248,7 +248,6 @@ public class MainActivity extends ToolbarActivity
 
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
